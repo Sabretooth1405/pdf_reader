c6c00aa05df3a4ce83b8e5eeef794e9d31d67883
sabretooth1405 <sabretooth1405@gmail.com>
2022-12-25 02:56:44 +0530

    fixed ui and added validation

--- pdf_reader/pdfs/models.py
+++ pdf_reader/pdfs/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
+import os
+from  django.core.exceptions import ValidationError
 
+def validate_file_extension(value):
+  
+  ext = os.path.splitext(value.name)[1]
+  valid_extensions = ['.pdf', '.jpg','.jpeg','.png','.webp','.tiff']
+  if not ext in valid_extensions:
+    raise ValidationError(u'File not supported!')
 
 class File(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     description = models.CharField(max_length=255, blank=True)
-    document = models.FileField(upload_to='documents/')
+    document = models.FileField(
+        upload_to='documents/', validators=[validate_file_extension])
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
